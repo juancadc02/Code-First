3f6163a5081bde97dae9558f4c02fd34d01ffd23
DESKTOP-NBUN2TS\Juanca <juancarlosdorado02c@gmail.com>
2023-10-18 19:11:27 +0200

    Agregar archivos de proyecto.

---
+++ Db/AplicacionDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Db.Modelo;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Db
+{
+    public class AplicacionDbContext : DbContext
+    {
+
+        public DbSet<LibrosDal> librosDals { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           
+                optionsBuilder.UseNpgsql("Server=localhost;Port=5432;Database=gestorBiblioteca;User Id=postgres;Password=Juanccaaa1992; SearchPath=public ");
+           
+        }
+    }
+}
---
+++ Db/Modelo/LibrosDal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Db.Modelo
+{
+    public  class LibrosDal
+    {
+        [Key]
+        
+        public int id_libro { get; set; }
+        public string nombre_libro { get; set; }
+    }
+}
